323f312c3fa07d1cadc37c0ccde3c3a05bb92f5c
Maxim <mpvpbalkon12@gmail.com>
2026-02-08 23:36:56 +0300

    add magic8

---
+++ magic8.py
@@ -0,0 +1,45 @@
+from random import*
+#magic8
+
+name = input() #"Леха Картоха"
+question = input()#"Ты когда-нибудь видел(а), чтобы снег падал вверх, а не вниз?"
+answer = ""
+random_number = randint(1,9)
+
+if random_number == 1:
+    answer = "Да, безусловно"
+
+elif random_number == 2:
+    answer = " Это решительно так"
+
+elif random_number == 3:
+    answer = "Без сомнения"
+
+elif random_number == 4:
+    answer = "Ответ туманный, попробуйте еще раз"
+
+elif random_number == 5:
+    answer = "Спросите еще раз позже"
+
+elif random_number == 6:
+    answer = "Лучше не говорить вам сейчас"
+
+elif random_number == 7:
+    answer = "Мои источники говорят «нет»"
+
+elif random_number == 8:
+    answer = "Прогноз не очень хороший"
+
+elif random_number == 9:
+    answer = "Очень сомнительно"
+else :
+    print("Ошибка")
+
+if name == "":
+    print("Вопрос:",question)
+else :
+    print(name,"спрашивает:",question)
+if question == "":
+    print("Magic 8-Ball не может принести удачу, иначе ткань реальности окажется под угрозой!")
+else :
+    print("Магический шар отвечает:",answer)
