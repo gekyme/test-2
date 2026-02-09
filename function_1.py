ae47a9cf8592a531c6ef65e3f8e8a3945a405acd
Maxim <mpvpbalkon12@gmail.com>
2026-02-09 21:13:32 +0300

    add func

---
+++ function_1.py
@@ -0,0 +1,3 @@
+def create_spreadsheet(title,row_coun = 1000):
+     print("Создание электронной таблицы с названием " + title + " с " + str(row_coun   ) + " строками")
+create_spreadsheet("Приложения",10)
